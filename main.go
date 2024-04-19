6e0dc1278d2239de89648d3034897d46b24fc456
renja <renja.grotemeyer@gmail.com>
2024-04-20 01:12:26 +0200

    Playig with gin

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -29,12 +31,99 @@ type Comment struct {
 	Content string `json:"content"`
 }
 
+// Get Articals from file
+func getArticlesFromFile() ([]Article, error) {
+	file, err := os.Open("articles.json")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	articles := []Article{}
+	err = json.NewDecoder(file).Decode(&articles)
+	if err != nil {
+		return nil, err
+	}
+	return articles, nil
+}
+
+// GetArticles
+func getArticles(c *gin.Context) {
+	articles, err := getArticlesFromFile()
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, articles)
+}
+
+// GetArticle by ID
+func getArticle(c *gin.Context) {
+	articleID := c.Param("articleID")
+	articles, err := getArticlesFromFile()
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
+		return
+	}
+
+	for _, article := range articles {
+		if article.ID == articleID {
+			c.IndentedJSON(http.StatusOK, article)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Article not found"})
+}
+
+// GetComments by article ID
+func getComments(c *gin.Context) {
+	articleID := c.Param("articleID")
+	articles, err := getArticlesFromFile()
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
+		return
+	}
+
+	for _, article := range articles {
+		if article.ID == articleID {
+			c.IndentedJSON(http.StatusOK, article.Comments)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Article not found"})
+}
+
+// GetComment by article ID and comment ID
+func getComment(c *gin.Context) {
+	articleID := c.Param("articleID")
+	commentID := c.Param("commentID")
+	articles, err := getArticlesFromFile()
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
+		return
+	}
+
+	for _, article := range articles {
+		if article.ID == articleID {
+			for _, comment := range article.Comments {
+				if comment.ID == commentID {
+					c.IndentedJSON(http.StatusOK, comment)
+					return
+				}
+			}
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Comment not found"})
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router.GET("/articles", getArticles)
+	router.GET("/articles/:articleID", getArticle)
+
+	router.GET("/articles/:articleID/comments", getComments)
+	router.GET("/articles/:articleID/comments/:commentID", getComment)
+
+	router.Run("localhost:8080")
 }
